41c1f8194ddf128afafbbea24356604aa959fa7e
Damien Corticchiato <skaradams69@gmail.com>
2019-12-05 18:28:45 +0100

    resolve day 1 - puzzle 2

--- day_1/solution.py
+++ day_1/solution.py
@@ -7,9 +7,23 @@ file = open(os.path.join(THIS_FOLDER, "input.txt"), 'r')
 def compute_mass(mass):
     return int((mass/3)) - 2
 
-result = 0
+# Part 1
+fuel = 0
 for line in file.readlines():
-    result += compute_mass(int(line))
+    fuel += compute_mass(int(line))
 
-print("Needed fuel : ")
+print("Needed fuel for modules : ")
+print(fuel)
+
+
+# Part 2
+result = fuel
+needed_fuel = 0
+while fuel > 0:
+    fuel = compute_mass(fuel)
+    needed_fuel += fuel
+    if(fuel > 0):
+        result += fuel
+
+print("Needed fuel for modules and fuel : ")
 print(result)
