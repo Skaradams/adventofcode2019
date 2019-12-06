54e06e9dcd62a705dde6d86a0e0057b86cadf9cb
Damien Corticchiato <skaradams69@gmail.com>
2019-12-06 22:38:25 +0100

    new solution for day 3

--- day_3/solution.py
+++ day_3/solution.py
@@ -11,45 +11,54 @@ cable2 = lines[1].split(",")
 def parse_instruction(instruction):
     return (instruction[0], int(instruction[1:]))
 
-def get_coordinates(instructions):
+
+def get_coordinates(instructions, other_coordinates=None):
     coordinates = [[0, 0]]
-    print('get coordinates')
+    intersections = []
+
     for instruction in instructions:
         instruction_data = parse_instruction(instruction)
         origin = coordinates[len(coordinates) - 1]
-
+        new_coordinates = (0,0)
         for i in range(instruction_data[1]):
             if(instruction_data[0] == 'R'):
-                coordinates.append([
+                new_coordinates = (
                     origin[0] + i,
                     origin[1]
-                ])
+                )
             elif(instruction_data[0] == 'L'):
-                coordinates.append([
+                new_coordinates = (
                     origin[0] - i,
                     origin[1]
-                ])
+                )
             elif(instruction_data[0] == 'U'):
-                coordinates.append([
+                new_coordinates = (
                     origin[0],
                     origin[1] + i
-                ])
+                )
             elif(instruction_data[0] == 'D'):
-                coordinates.append([
+                new_coordinates = (
                     origin[0],
                     origin[1] - i
-                ])
+                )
+        coordinates.append(new_coordinates)
 
-    return coordinates
+        if(other_coordinates and new_coordinates in other_coordinates):
+            print("qsdhgqsjdhq")
+            intersections.append(new_coordinates)
+
+    return (coordinates, intersections)
 
 def compare_coordinates(coordinates1, coordinates2):
     common_coordinates = []
+
     for coordinate in coordinates2:
         if(coordinate in coordinates1):
             common_coordinates.append(coordinate)
     return common_coordinates
 
-print(compare_coordinates(
-    get_coordinates(cable1),
-    get_coordinates(cable2),
-))
+
+(a, intersections) = get_coordinates(cable1)
+(b, intersections) = get_coordinates(cable2, a)
+
+print(intersections)
